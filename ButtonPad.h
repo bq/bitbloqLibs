87092018d5584fcfb3b49ddda8c99ab838799b09
Irene Sanz <irene.sanz@bq.com>
2015-07-28 23:12:00 +0200

    ButtonPad read and readReturnNum fixed. Now read function returns the id character of the pad pressed

--- ButtonPad.h
+++ ButtonPad.h
@@ -8,15 +8,17 @@ class ButtonPad
     ButtonPad(int pin);
     ButtonPad(int pin, int num_keys, int * adc_key_val);
 
-    int read();
+    int readReturnNum();
+    char read();
     
   private:
     int get_key(int input);
     int _pin;
     int _num_keys;
     int * _adc_key_val;
-	int _key;
-	int _oldkey;
+  int _key;
+  int _oldkey;
+  char * _keys;
 
 };
 
