f479c7d7a02bcd263bd16e231bbaab770d8997a2
Irene Sanz <irene.sanz@bq.com>
2015-10-19 15:48:34 +0200

    Removed sound and crash sensor since this wrapping increases dramatically the .hex size

--- CrashSensor.cpp
+++
@@ -1,9 +0,0 @@
-#include "CrashSensor.h"
-//****** CrashSensor ******//
-CrashSensor::CrashSensor(int pinSensor){
-  _pinSensor = pinSensor;
-}
-
-int CrashSensor::readCrashSensor(){
-  return digitalRead(_pinSensor);
-}
--- SoundSensor.cpp
+++
@@ -1,9 +0,0 @@
-#include "SoundSensor.h"
-//****** SoundSensor ******//
-SoundSensor::SoundSensor(int pinSensor){
-  _pinSensor = pinSensor;
-}
-
-int SoundSensor::readSoundSensor(){
-  return digitalRead(_pinSensor);
-}
