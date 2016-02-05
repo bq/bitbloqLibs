eaf6b2e600ca950b30d9c53aeabe68e374f6c1df
SergioMatebq <sergio.mate@bq.com>
2016-02-05 15:14:01 +0100

    BMP180 lib changed

--- BitbloqBMP180/BitbloqBMP180.cpp
+++ BitbloqBMP180/BitbloqBMP180.cpp
@@ -1,120 +1,191 @@
 #include "BitbloqBMP180.h"
-#include "Wire.h"
-#include "Arduino.h"
 
-void BMP180::init(){
+
+void BMP180::init(uint8_t mode) {
+  if (mode > BMP180_ULTRAHIGHRES) 
+    mode = BMP180_ULTRAHIGHRES;
+  oversampling = mode;
+
   Wire.begin();
-  ac1 = read16b(0xAA);
-  ac2 = read16b(0xAC);
-  ac3 = read16b(0xAE);
-  ac4 = read16b(0xB0);
-  ac5 = read16b(0xB2);
-  ac6 = read16b(0xB4);
-  b1 = read16b(0xB6);
-  b2 = read16b(0xB8);
-  mb = read16b(0xBA);
-  mc = read16b(0xBC);
-  md = read16b(0xBE);
+
+  /* read calibration data */
+  ac1 = read16(BMP180_CAL_AC1);
+  ac2 = read16(BMP180_CAL_AC2);
+  ac3 = read16(BMP180_CAL_AC3);
+  ac4 = read16(BMP180_CAL_AC4);
+  ac5 = read16(BMP180_CAL_AC5);
+  ac6 = read16(BMP180_CAL_AC6);
+
+  b1 = read16(BMP180_CAL_B1);
+  b2 = read16(BMP180_CAL_B2);
+
+  mb = read16(BMP180_CAL_MB);
+  mc = read16(BMP180_CAL_MC);
+  md = read16(BMP180_CAL_MD);
+
   sealevelPressure = 101325;
 }
 
-
-float BMP180::getTemperature(){
-  float temp;
-
-  temp = (TemperatureCompensation() + 8)>>4;
-  temp /= 10;
-
-  return temp;
+int32_t BMP180::computeB5(int32_t UT) {
+  int32_t X1 = (UT - (int32_t)ac6) * ((int32_t)ac5) >> 15;
+  int32_t X2 = ((int32_t)mc << 11) / (X1+(int32_t)md);
+  return X1 + X2;
 }
 
-float BMP180::getPressure(){
-  long PressureCompensation, x1, x2, x3, b3, b6, p;
-  unsigned long b4, b7;
-
-  Wire.beginTransmission(BMP180_ADDRESS);
-  Wire.write(CONTROL);
-  Wire.write(PRESCMD + (oversampling<<6));
-  Wire.endTransmission();
-  delay(2 + (3<<oversampling));
-
-  PressureCompensation = read16b(DATA);
-  PressureCompensation <<= 8;
-  PressureCompensation |= read8b(DATA+2);
-  PressureCompensation >>= (8 - oversampling);
-
-  b6 = TemperatureCompensation() - 4000L;
-  x1 = ((long)b2 * (b6 * b6)>>12)>>11;
-  x2 = ((long)ac2 * b6)>>11;
-  x3 = x1 + x2;
-  b3 = (((((long)ac1)*4 + x3)<<oversampling) + 2)>>2;
-
-  x1 = ((long)ac3 * b6)>>13;
-  x2 = ((long)b1 * ((b6 * b6)>>12))>>16;
-  x3 = ((x1 + x2) + 2)>>2;
-  b4 = ((unsigned long)ac4 * (unsigned long)(x3 + 32768))>>15;
-  b7 = ((unsigned long)PressureCompensation - b3) * (unsigned long)(50000UL>>oversampling);
-
-  if (b7 < 0x80000000)
-    p = (b7<<1)/b4;
-  else
-    p = (b7/b4)<<1;
-
-  x1 = (p>>8) * (p>>8);
-  x1 = (x1 * 3038)>>16;
-  x2 = (-7357 * p)>>16;
-  p += (x1 + x2 + 3791UL)>>4;
-
-  return (float) p;
+uint16_t BMP180::readRawTemperature(void) {
+  write8(BMP180_CONTROL, BMP180_READTEMPCMD);
+  delay(5);
+  return read16(BMP180_TEMPDATA);
 }
 
-float BMP180::getAltitude(){
-  return 44307.69396 * (1.0 - pow(getPressure()/sealevelPressure, 0.190284));
+uint32_t BMP180::readRawPressure(void) {
+  uint32_t raw;
+
+  write8(BMP180_CONTROL, BMP180_READPRESSURECMD + (oversampling << 6));
+
+  if (oversampling == BMP180_ULTRALOWPOWER) 
+    delay(5);
+  else if (oversampling == BMP180_STANDARD) 
+    delay(8);
+  else if (oversampling == BMP180_HIGHRES) 
+    delay(14);
+  else 
+    delay(26);
+
+  raw = read16(BMP180_PRESSUREDATA);
+
+  raw <<= 8;
+  raw |= read8(BMP180_PRESSUREDATA+2);
+  raw >>= (8 - oversampling);
+
+  return raw;
 }
 
+
+int32_t BMP180::getPressure() {
+  int32_t UT, UP, B3, B5, B6, X1, X2, X3, p;
+  uint32_t B4, B7;
+
+  UT = readRawTemperature();
+  UP = readRawPressure();
+
+  B5 = computeB5(UT);
+
+  // do pressure calcs
+  B6 = B5 - 4000;
+  X1 = ((int32_t)b2 * ( (B6 * B6)>>12 )) >> 11;
+  X2 = ((int32_t)ac2 * B6) >> 11;
+  X3 = X1 + X2;
+  B3 = ((((int32_t)ac1*4 + X3) << oversampling) + 2) / 4;
+
+  X1 = ((int32_t)ac3 * B6) >> 13;
+  X2 = ((int32_t)b1 * ((B6 * B6) >> 12)) >> 16;
+  X3 = ((X1 + X2) + 2) >> 2;
+  B4 = ((uint32_t)ac4 * (uint32_t)(X3 + 32768)) >> 15;
+  B7 = ((uint32_t)UP - B3) * (uint32_t)( 50000UL >> oversampling );
+
+  if (B7 < 0x80000000) {
+    p = (B7 * 2) / B4;
+  } else {
+    p = (B7 / B4) * 2;
+  }
+  X1 = (p >> 8) * (p >> 8);
+  X1 = (X1 * 3038) >> 16;
+  X2 = (-7357 * p) >> 16;
+
+  p = p + ((X1 + X2 + (int32_t)3791)>>4);
+
+  return p;
+}
 
 void BMP180::setSealevelPressure(float pressure){
   sealevelPressure = pressure;
 }
 
+float BMP180::getTemperature() {
+  int32_t UT, B5;     // following ds convention
+  float temp;
 
-int BMP180::read16b(unsigned char address){
-	unsigned char data;
+  UT = readRawTemperature();
 
-  Wire.beginTransmission(BMP180_ADDRESS);
-  Wire.write(address);
-  Wire.endTransmission();
+  B5 = computeB5(UT);
+  temp = (B5+8) >> 4;
+  temp /= 10;
   
-  Wire.requestFrom(BMP180_ADDRESS, 2);
-  while(Wire.available()<2);
-  data = Wire.read();
-  data <<= 8;
-  data |= Wire.read();
-
-  return (int) data;
+  return temp;
 }
 
-char BMP180::read8b(unsigned char address){
-  Wire.beginTransmission(BMP180_ADDRESS);
-  Wire.write(address);
-  Wire.endTransmission();
-  
-  Wire.requestFrom(BMP180_ADDRESS, 1);
-  while(Wire.available());
-  return Wire.read();
+float BMP180::getAltitude() {
+  float altitude;
+
+  float pressure = getPressure();
+
+  altitude = 44330 * (1.0 - pow(pressure /sealevelPressure,0.1903));
+
+  return altitude;
 }
 
-long BMP180::TemperatureCompensation(){
-  long x1, x2;
 
-  Wire.beginTransmission(BMP180_ADDRESS);
-  Wire.write(CONTROL);
-  Wire.write(TEMPCMD);
-  Wire.endTransmission();
-  delay(5);
+/*********************************************************************/
 
-  x1 = (((long)read16b(DATA) - (long)ac6)*(long)ac5) >> 15;
-  x2 = ((long)mc << 11)/(x1 + md);
+uint8_t BMP180::read8(uint8_t a) {
+  uint8_t ret;
 
-  return x1 + x2;
+  Wire.beginTransmission(BMP180_I2CADDR); // start transmission to device 
+#if (ARDUINO >= 100)
+  Wire.write(a); // sends register address to read from
+#else
+  Wire.send(a); // sends register address to read from
+#endif
+  Wire.endTransmission(); // end transmission
+  
+  Wire.beginTransmission(BMP180_I2CADDR); // start transmission to device 
+  Wire.requestFrom(BMP180_I2CADDR, 1);// send data n-bytes read
+#if (ARDUINO >= 100)
+  ret = Wire.read(); // receive DATA
+#else
+  ret = Wire.receive(); // receive DATA
+#endif
+  Wire.endTransmission(); // end transmission
+
+  return ret;
+}
+
+uint16_t BMP180::read16(uint8_t a) {
+  uint16_t ret;
+
+  Wire.beginTransmission(BMP180_I2CADDR); // start transmission to device 
+#if (ARDUINO >= 100)
+  Wire.write(a); // sends register address to read from
+#else
+  Wire.send(a); // sends register address to read from
+#endif
+  Wire.endTransmission(); // end transmission
+  
+  Wire.beginTransmission(BMP180_I2CADDR); // start transmission to device 
+  Wire.requestFrom(BMP180_I2CADDR, 2);// send data n-bytes read
+#if (ARDUINO >= 100)
+  ret = Wire.read(); // receive DATA
+  ret <<= 8;
+  ret |= Wire.read(); // receive DATA
+#else
+  ret = Wire.receive(); // receive DATA
+  ret <<= 8;
+  ret |= Wire.receive(); // receive DATA
+#endif
+  Wire.endTransmission(); // end transmission
+
+  return ret;
+}
+
+void BMP180::write8(uint8_t a, uint8_t d) {
+  Wire.beginTransmission(BMP180_I2CADDR); // start transmission to device 
+#if (ARDUINO >= 100)
+  Wire.write(a); // sends register address to read from
+  Wire.write(d);  // write data
+#else
+  Wire.send(a); // sends register address to read from
+  Wire.send(d);  // write data
+#endif
+  Wire.endTransmission(); // end transmission
 }
