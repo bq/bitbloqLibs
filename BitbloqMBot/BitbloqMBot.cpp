d4b1168f610859262c1865cbe75f4475ea90799f
Tom Calvo <calvo.tom@gmail.com>
2016-10-10 19:35:55 +0900

    mBot functions

--- BitbloqMBot/BitbloqMBot.cpp
+++ BitbloqMBot/BitbloqMBot.cpp
@@ -6,12 +6,28 @@
 
 MBot::MBot(){
 	boardLeds = new MeRGBLed(7);
+	buzzer = new MeBuzzer(8);
+	lightSensor = new MeLightSensor(6);
+	leftMotor = new MeDCMotor(9);
+	rightMotor = new MeDCMotor(10);
 }
 
 MBot::~MBot(){
 	if (boardLeds != NULL){
 		delete boardLeds;
 		boardLeds = NULL;
+	}
+	if (buzzer != NULL){
+		delete buzzer;
+		buzzer = NULL;
+	}
+	if (ultrasonicSensor != NULL){
+		delete ultrasonicSensor;
+		ultrasonicSensor = NULL;
+	}
+	if (lightSensor != NULL){
+		delete lightSensor;
+		lightSensor = NULL;
 	}
 }
 
@@ -22,4 +38,42 @@ void MBot::init(){
 void MBot::setLed(int led, int red, int green, int blue){
     boardLeds->setColor(led, red, green, blue);
     boardLeds->show();
+}
+
+void MBot::tone(int note, int beat){
+    buzzer->tone(note, beat);
+}
+
+int MBot::getDistance(int port){
+	ultrasonicSensor = new MeUltrasonicSensor(port);
+	return ultrasonicSensor.distanceCm();
+}
+
+int MBot::getButtonStatus(){
+	return analogRead(A7);
+}
+
+int MBot::getLightSensor(){
+	return lightSensor.read();
+}
+
+void MBot::move(int direction, int speed){
+	int leftSpeed = 0;
+	int rightSpeed = 0;
+	if(direction == 1){
+		leftSpeed = speed;
+		rightSpeed = speed;
+	}else if(direction == 2){
+		leftSpeed = -speed;
+		rightSpeed = -speed;
+	}else if(direction == 3){
+		leftSpeed = -speed;
+		rightSpeed = speed;
+	}else if(direction == 4){
+		leftSpeed = speed;
+		rightSpeed = -speed;
+	}
+	leftMotor.run(leftSpeed);
+	rightMotor.run(rightSpeed);
+
 }
--- BitbloqMBot/BitbloqMBot.h
+++ BitbloqMBot/BitbloqMBot.h
@@ -1,13 +1,29 @@
 class MeRGBLed;
+class MeBuzzer;
+class MeUltrasonicSensor;
+class MeLightSensor;
+class MeDCMotor;
 
 class MBot
 {
-public:
-	MBot();
-	virtual ~MBot();
 
-    void init();
-    void setLed(int led, int red, int green, int blue);
-private:
-	MeRGBLed* boardLeds;
+	public:
+		MBot();
+		virtual ~MBot();
+
+	    void init();
+	    void setLed(int led, int red, int green, int blue);
+	    void tome();
+	    int getDistance();
+	    int getButtonStatus();
+	    int getLightSensor();
+	    void move(int direction, int speed);
+
+	private:
+		MeRGBLed* boardLeds;
+		MeBuzzer* buzzer;
+		MeUltrasonicSensor* ultrasonicSensor;
+		MeLightSensor* lightSensor;
+		MeDCMotor* leftMotor;
+		MeDCMotor* rightMotor;
 };
