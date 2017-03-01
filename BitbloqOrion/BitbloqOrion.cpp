0307f61a4154a27dcd3e058d3a69cdc074db5b43
Tom Calvo <calvo.tom@gmail.com>
2017-03-01 16:53:13 +0100

    change ports order to reuse components bloqs

--- BitbloqOrion/BitbloqOrion.cpp
+++ BitbloqOrion/BitbloqOrion.cpp
@@ -2,7 +2,7 @@
 #include "BitbloqOrion.h"
 
 //setting Orion ports values
-const Port BitbloqOrion::ports[9] = {Port(0,0),Port(10,11),Port(9,3),Port(13,12),Port(2,8),Port(0,1),Port(A3,A2),Port(A1,A6),Port(A0,A7)};
+const Port BitbloqOrion::ports[9] = {Port(0,0),Port(10,11),Port(9,3),Port(12,13),Port(8,2),Port(0,1),Port(A2,A3),Port(A6,A1),Port(A7,A0)};
 
 BitbloqOrion::BitbloqOrion():
         buzzerPin(8),
