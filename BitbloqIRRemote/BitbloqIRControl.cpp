68a7967e9a90684b548a46d4fff869882d4a8dba
Álvaro Sánchez Bravo <alvaro.sanchez@bq.com>
2017-06-09 09:41:58 +0200

    Added IR commands for Makeblock

--- BitbloqIRRemote/BitbloqIRControl.cpp
+++ BitbloqIRRemote/BitbloqIRControl.cpp
@@ -1,24 +1,24 @@
 /*
  * BitbloqIRControl.cpp
- * 
+ *
  * Copyright 2017 Alberto Valero <avalero.valero@bq.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
 */
 
 #include "BitbloqIRControl.h"
@@ -44,12 +44,12 @@ long int BitbloqIRControl::getRaw(){
             {
                 _InfraredControlCommand1=_InfraredControlCommand2;
             }
-          
+
         }
         return _InfraredControlCommand2;
     }
-    
-}	
+
+}
 
 char BitbloqIRControl::getInfraredControlCommand(){
 	IRrecv _InfraredReceiveCommand(receiverPin);
@@ -68,60 +68,73 @@ char BitbloqIRControl::getInfraredControlCommand(){
                 case -1497820227:
                 return 'F'; //forward
                 break;
-            
+
                 case 1113853081:
                 return 'B'; //backward
                 break;
-            
+
                 case 32049505:
-                return 'L'; //left 
+								case -260652741:
+                return 'L'; //left
                 break;
-                
+
                 case -885166211:
+								case -439370369:
                 return 'R'; //right
                 break;
-            
+
                 case -1839375523:
+								case -1547112997:
                 return 'S'; //OK
                 break;
 
                 case 1811778305:
+								case -1056840325:
                 return '0';
                 break;
 
                 case -555408703:
+								case -1760117185:
                 return '1';
                 break;
 
                 case 1868133565:
+								case 1033561079:
                 return '2';
                 break;
 
                 case -1737510107:
+								case 1635910171:
                 return '3';
                 break;
 
                 case -875377411:
+								case -1943902853:
                 return '4';
                 break;
 
                 case 629155073:
+								case 1217346747:
                 return '5';
                 break;
 
                 case -2048877215:
+								case 71952287:
                 return '6';
                 break;
 
                 case -949029659:
+								case 851901943:
                 return '7';
                 break;
 
                 case 1474512609:
+								case 465573243:
                 return '8';
                 break;
 
                 case -1866245375:
+								case 1053031451:
                 return '9';
                 break;
 
@@ -144,7 +157,39 @@ char BitbloqIRControl::getInfraredControlCommand(){
                 case -1292968131: //Power ON/FF
                 return 'P';
                 break;
-                
+
+								case -672642277:
+								return 'U';
+								break;
+
+								case -1756873733:
+								return 'G';
+								break;
+
+								case -484956645:
+								return 'A';
+								break;
+
+								case 5316027:
+								return 'B';
+								break;
+
+								case -293048961:
+								return 'C';
+								break;
+
+								case 1386468383:
+								return 'D';
+								break;
+
+								case 553536955:
+								return 'E';
+								break;
+
+								case -255584701:
+								return 'F';
+								break;
+
                 default:
                 return 'T';
                 break;
