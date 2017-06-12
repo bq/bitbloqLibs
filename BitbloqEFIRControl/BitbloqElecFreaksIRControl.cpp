711bbe4e2b70329470f0e21a5c44129d08087269
Álvaro Sánchez Bravo <alvaro.sanchez@bq.com>
2017-06-12 11:36:23 +0200

    Changed IR controls in freakscar

--- BitbloqEFIRControl/BitbloqElecFreaksIRControl.cpp
+++ BitbloqEFIRControl/BitbloqElecFreaksIRControl.cpp
@@ -43,71 +43,93 @@ char ElecfreaksIRControl::getInfraredControlCommand(){
             }
             switch(_InfraredControlCommand2)
     		{
-     			case 0xFDA05F:
+     			case 0xFDA05F: //up
+					case -1497820227:
     			return 'F';
     			break;
-		   	
-      			case 0xFDB04F:
+
+      			case 0xFDB04F: //down
+						case 1113853081:
       			return 'D';
       			break;
-		   	
-      			case 0xFD10EF:
+
+      			case 0xFD10EF: //left
+						case 32049505:
       			return 'L';
       			break;
-				
-      			case 0xFD50AF:
+
+      			case 0xFD50AF: //right
+						case -885166211:
       			return 'R';
       			break;
-		   	
-      			case 0xFD906F:
+
+      			case 0xFD906F: //ok
+						case -1839375523:
 		   	 	return 'S';
      			break;
 
+
+											case -799895003: //return
+					      			return 'X';
+					      			break;
+
 				case 0xFD708F:
+				case 1811778305:
 				return '0';
 				break;
 
 				case 0xFD08F7:
+				case -555408703:
 				return '1';
 				break;
 
 				case 0xFD8877:
+				case 1868133565:
 				return '2';
 				break;
 
 				case 0xFD48B7:
+				case -1737510107:
 				return '3';
 				break;
 
 				case 0xFD28D7:
+				case -875377411:
 				return '4';
 				break;
 
 				case 0xFDA857:
+				case 629155073:
 				return '5';
 				break;
 
 				case 0xFD6897:
+				case -2048877215:
 				return '6';
 				break;
 
 				case 0xFD18E7:
+				case -949029659:
 				return '7';
 				break;
 
 				case 0xFD9867:
+				case 1474512609:
 				return '8';
 				break;
 
 				case 0xFD58A7:
+				case -1866245375:
 				return '9';
 				break;
 
 				case 0xFD20DF:
+				case 373604801:
 				return '+';
 				break;
 
 				case 0xFD30CF:
+				case -1309689187:
 				return '-';
 				break;
 
@@ -115,18 +137,20 @@ char ElecfreaksIRControl::getInfraredControlCommand(){
 				return 'B';
 				break;
 
-				case 0xFD40BF:
+				case 0xFD40BF: //menu
+				case 46690913:
 				return 'M';
 				break;
 
-				case 0xFD00FF:
+				case 0xFD00FF: //on-off
+				case -1292968131:
 				return 'P';
 				break;
-				
+
 				default:
 				return 'T';
 				break;
-				
+
     		}
         }
     }
