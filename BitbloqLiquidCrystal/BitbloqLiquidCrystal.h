9f0e433e5cec552397e82557d8132ac3f96a4857
Alberto Valero <avalero@bq.com>
2017-12-11 13:30:43 +0100

    Added Adafruit NeoPixel library

--- BitbloqLiquidCrystal/BitbloqLiquidCrystal.h
+++ BitbloqLiquidCrystal/BitbloqLiquidCrystal.h
@@ -1,3 +1,25 @@
+/*
+ * BitbloqLiquidCrystal.h
+ * 
+ * Copyright 2017 Alberto Valero <avalero@bq>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
 #ifndef LiquidCrystal_h
 #define LiquidCrystal_h
 
@@ -121,5 +143,89 @@ private:
   uint8_t _i2cAddr;
   MCP23008 _i2c;
 };
+
+
+namespace Bitbloq {
+
+class LiquidCrystal : public Print {
+public:
+  LiquidCrystal(uint8_t rs, uint8_t enable,
+		uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+		uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+  LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
+		uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+		uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+  LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
+		uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+  LiquidCrystal(uint8_t rs, uint8_t enable,
+		uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+
+  LiquidCrystal(uint8_t i2cAddr);
+  LiquidCrystal(uint8_t data, uint8_t clock, uint8_t latch);
+
+  void init(uint8_t fourbitmode, uint8_t rs, uint8_t rw, uint8_t enable,
+	    uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+	    uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+
+  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+
+  void clear();
+  void home();
+
+  void noDisplay();
+  void display();
+  void noBlink();
+  void blink();
+  void noCursor();
+  void cursor();
+  void scrollDisplayLeft();
+  void scrollDisplayRight();
+  void leftToRight();
+  void rightToLeft();
+  void autoscroll();
+  void noAutoscroll();
+
+  // only if using backpack
+  void setBacklight(uint8_t status);
+
+  void createChar(uint8_t, uint8_t[]);
+  void setCursor(uint8_t, uint8_t);
+  virtual  size_t write(uint8_t);
+  void command(uint8_t);
+
+  void printTextHorizontal(String message, int delay_num);
+  void printTextVertical(String message, int delay_num, bool completeWords);
+
+private:
+  void send(uint8_t, uint8_t);
+  void write4bits(uint8_t);
+  void write8bits(uint8_t);
+  void pulseEnable();
+  void _digitalWrite(uint8_t, uint8_t);
+  void _pinMode(uint8_t, uint8_t);
+
+  uint8_t _columns;
+  uint8_t _rows;
+  uint8_t _rs_pin; // LOW: command.  HIGH: character.
+  uint8_t _rw_pin; // LOW: write to LCD.  HIGH: read from LCD.
+  uint8_t _enable_pin; // activated by a HIGH pulse.
+  uint8_t _data_pins[8];
+
+  uint8_t _displayfunction;
+  uint8_t _displaycontrol;
+  uint8_t _displaymode;
+
+  uint8_t _initialized;
+
+  uint8_t _numlines,_currline;
+
+  uint8_t _SPIclock, _SPIdata, _SPIlatch;
+  uint8_t _SPIbuff;
+
+  uint8_t _i2cAddr;
+  MCP23008 _i2c;
+};
+	
+} //end namespace
 
 #endif
