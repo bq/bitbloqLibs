33fb81da8c3a0b58e932e7a4f01836fca1944dfa
avalero <alberto.valero@bq.com>
2018-01-21 17:29:54 +0100

    I2C 7 Segment Display for ZUM Junior added and tested. See examples folder

---
+++ BitbloqI2C7SegmentDisplay/examples/ZUM_Junior_7Segment/ZUM_Junior_7Segment.ino
@@ -0,0 +1,31 @@
+#include <BitbloqI2C7SegmentDisplay.h>
+
+Bitbloq::ZUMJunior::I2C7SegmentDisplay SegmentDisplay;
+char c_caracter = 48;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  SegmentDisplay.begin(1);
+
+  SegmentDisplay.displayChar('b','q');
+
+  delay(1000);
+
+  SegmentDisplay.displayChar(' ',' ');
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  SegmentDisplay.displayChar(c_caracter,c_caracter+1);
+  c_caracter++;
+  if ((c_caracter > 57) & (c_caracter < 65)){
+    c_caracter = 65;  
+  }
+  if (c_caracter > 90){
+    c_caracter = 48;  
+  }
+  Serial.println(c_caracter);
+  delay(500);
+}
---
+++ BitbloqI2C7SegmentDisplay/src/BitbloqI2C7SegmentDisplay.cpp
@@ -0,0 +1,399 @@
+/***************************************************
+
+	BQ -- ZUM Junior 7 Segment Display TCA9535
+	
+***************************************************/
+#define DEBUG
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+#include <BitbloqI2C7SegmentDisplay.h>
+
+namespace Bitbloq{ namespace ZUMJunior{
+
+
+// Instantiate I2C7SegmentDisplay class
+I2C7SegmentDisplay::I2C7SegmentDisplay(void)
+{
+}
+
+SegmentStat I2C7SegmentDisplay::begin(uint8_t ui8_i2cport)
+{
+	uint8_t ui8_Data = 0;
+	uint8_t ui8_Conf = 0;
+	
+	segmentdisplay_i2cport = ui8_i2cport;
+	
+	switch (segmentdisplay_i2cport) {
+		case 0:
+			Wire.begin();
+			break;
+		case 1:
+			Wire1.begin();
+			break;
+		default:
+			return SEGMENT_NOK_i2cport;
+	}
+	
+	ui8_Data = TCA9535_CP_B0_OUT | TCA9535_CP_B1_OUT | TCA9535_CP_B2_OUT | TCA9535_CP_B3_OUT | TCA9535_CP_B4_OUT | TCA9535_CP_B5_OUT | TCA9535_CP_B6_OUT | TCA9535_CP_B7_OUT;
+	writeRegister(TCA9535_REG_CP0, ui8_Data);
+	ui8_Conf = readRegister(TCA9535_REG_CP0);
+	if (ui8_Conf != ui8_Data)
+	{
+		return SEGMENT_NOK_config;
+	}	
+	
+	ui8_Data = TCA9535_CP_B0_OUT | TCA9535_CP_B1_OUT | TCA9535_CP_B2_OUT | TCA9535_CP_B3_OUT | TCA9535_CP_B4_OUT | TCA9535_CP_B5_OUT | TCA9535_CP_B6_OUT | TCA9535_CP_B7_OUT;
+	writeRegister(TCA9535_REG_CP1, ui8_Data);
+	ui8_Conf = readRegister(TCA9535_REG_CP1);
+	if (ui8_Conf != ui8_Data)
+	{
+		return SEGMENT_NOK_config;
+	}
+	
+	return SEGMENT_OK;
+}
+
+void I2C7SegmentDisplay::display(uint8_t i_disp1, uint8_t i_disp0)
+{
+	writeRegister(TCA9535_REG_OP0, i_disp0);
+	writeRegister(TCA9535_REG_OP1, i_disp1);
+}
+
+void I2C7SegmentDisplay::displayChar(char c_disp1, char c_disp0)
+{
+	switch (c_disp1) {
+		case '0':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_0);
+			break;
+		case '1':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_1);
+			break;
+		case '2':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_2);
+			break;
+		case '3':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_3);
+			break;			
+		case '4':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_4);
+			break;			
+		case '5':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_5);
+			break;			
+		case '6':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_6);
+			break;			
+		case '7':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_7);
+			break;			
+		case '8':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_8);
+			break;			
+		case '9':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_9);
+			break;			
+		case 'a':
+		case 'A':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_A);
+			break;
+		case 'b':
+		case 'B':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_B);
+			break;
+		case 'c':
+		case 'C':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_C);
+			break;
+		case 'd':
+		case 'D':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_D);
+			break;			
+		case 'e':
+		case 'E':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_E);
+			break;			
+		case 'f':
+		case 'F':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_F);
+			break;			
+		case 'g':
+		case 'G':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_G);
+			break;			
+		case 'h':
+		case 'H':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_H);
+			break;			
+		case 'i':
+		case 'I':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_I);
+			break;			
+		case 'j':
+		case 'J':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_J);
+			break;
+		case 'k':
+		case 'K':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_K);
+			break;	
+		case 'l':
+		case 'L':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_L);
+			break;	
+		case 'm':
+		case 'M':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_M);
+			break;	
+		case 'n':
+		case 'N':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_N);
+			break;	
+		case 'o':
+		case 'O':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_O);
+			break;	
+		case 'p':
+		case 'P':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_P);
+			break;	
+		case 'q':
+		case 'Q':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_Q);
+			break;	
+		case 'r':
+		case 'R':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_R);
+			break;	
+		case 's':
+		case 'S':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_S);
+			break;	
+		case 't':
+		case 'T':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_T);
+			break;	
+		case 'u':
+		case 'U':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_U);
+			break;	
+		case 'v':
+		case 'V':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_V);
+			break;	
+		case 'w':
+		case 'W':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_W);
+			break;	
+		case 'x':
+		case 'X':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_X);
+			break;	
+		case 'y':
+		case 'Y':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_Y);
+			break;	
+		case 'z':
+		case 'Z':
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_Z);
+			break;	
+		default:
+			writeRegister(TCA9535_REG_OP1, TCA9535_CHAR_BLANK);
+			break;
+	}
+			
+	switch (c_disp0) {
+		case '0':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_0);
+			break;
+		case '1':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_1);
+			break;
+		case '2':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_2);
+			break;
+		case '3':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_3);
+			break;			
+		case '4':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_4);
+			break;			
+		case '5':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_5);
+			break;			
+		case '6':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_6);
+			break;			
+		case '7':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_7);
+			break;			
+		case '8':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_8);
+			break;			
+		case '9':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_9);
+			break;			
+		case 'a':
+		case 'A':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_A);
+			break;
+		case 'b':
+		case 'B':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_B);
+			break;
+		case 'c':
+		case 'C':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_C);
+			break;
+		case 'd':
+		case 'D':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_D);
+			break;			
+		case 'e':
+		case 'E':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_E);
+			break;			
+		case 'f':
+		case 'F':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_F);
+			break;			
+		case 'g':
+		case 'G':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_G);
+			break;			
+		case 'h':
+		case 'H':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_H);
+			break;			
+		case 'i':
+		case 'I':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_I);
+			break;			
+		case 'j':
+		case 'J':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_J);
+			break;
+		case 'k':
+		case 'K':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_K);
+			break;	
+		case 'l':
+		case 'L':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_L);
+			break;	
+		case 'm':
+		case 'M':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_M);
+			break;	
+		case 'n':
+		case 'N':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_N);
+			break;	
+		case 'o':
+		case 'O':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_O);
+			break;	
+		case 'p':
+		case 'P':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_P);
+			break;	
+		case 'q':
+		case 'Q':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_Q);
+			break;	
+		case 'r':
+		case 'R':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_R);
+			break;	
+		case 's':
+		case 'S':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_S);
+			break;	
+		case 't':
+		case 'T':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_T);
+			break;	
+		case 'u':
+		case 'U':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_U);
+			break;	
+		case 'v':
+		case 'V':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_V);
+			break;	
+		case 'w':
+		case 'W':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_W);
+			break;	
+		case 'x':
+		case 'X':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_X);
+			break;	
+		case 'y':
+		case 'Y':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_Y);
+			break;	
+		case 'z':
+		case 'Z':
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_Z);
+			break;	
+		default:
+			writeRegister(TCA9535_REG_OP0, TCA9535_CHAR_BLANK);
+			break;
+	}			
+}
+
+void I2C7SegmentDisplay::writeRegister(uint8_t ui8_Reg, uint8_t ui8_data)
+{
+	switch (segmentdisplay_i2cport) {
+		case 0:
+			Wire.beginTransmission(TCA9535_ADDR);
+			Wire.write((byte)ui8_Reg);
+			Wire.write((byte)ui8_data);
+			Wire.endTransmission(false);
+			break;
+		case 1:
+			Wire1.beginTransmission(TCA9535_ADDR);
+			Wire1.write((byte)ui8_Reg);
+			Wire1.write((byte)ui8_data);
+			Wire1.endTransmission(false);
+			break;
+		default:
+			return;
+	}
+
+}
+
+uint8_t I2C7SegmentDisplay::readRegister(uint8_t ui8_Reg)
+{
+	uint8_t ui8_data = 0;
+  
+	switch (segmentdisplay_i2cport) {
+		case 0:
+			Wire.beginTransmission(TCA9535_ADDR);
+			Wire.write((byte)ui8_Reg);
+			Wire.endTransmission(false);
+			Wire.requestFrom((byte)TCA9535_ADDR, (byte)1);
+			ui8_data = Wire.read();
+			break;		
+		case 1:
+			Wire1.beginTransmission(TCA9535_ADDR);
+			Wire1.write((byte)ui8_Reg);
+			Wire1.endTransmission(false);
+			Wire1.requestFrom((byte)TCA9535_ADDR, (byte)1);
+			ui8_data = Wire1.read();
+			break;
+		default:
+			return 0;
+	}
+  
+  return ui8_data;
+}
+
+}} //end namespace Bitbloq::ZUMJunior
